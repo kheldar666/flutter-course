7dea820715be0e9b9bf6d6a48c2e3445bc5f519d
Martin Papy <martin.papy@gmail.com>
2021-11-15 16:17:47 +0700

    Failing due to old project structure

--- xylophone-flutter/lib/main.dart
+++ xylophone-flutter/lib/main.dart
@@ -1,16 +1,28 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  final player = AudioCache(prefix: 'assets/');
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Center(
+            child: TextButton(
+              onPressed: playSound(),
+              child: Text('Click Me'),
+            ),
+          ),
         ),
       ),
     );
+  }
+
+  playSound() {
+    player.play('note1.wav');
   }
 }
