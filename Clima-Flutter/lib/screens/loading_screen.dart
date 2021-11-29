a9fdff29e5db1bfea6be3093b214108421b126a7
Martin Papy <martin.papy@gmail.com>
2021-11-29 12:58:21 +0700

    Working on Climat App

--- Clima-Flutter/lib/screens/loading_screen.dart
+++ Clima-Flutter/lib/screens/loading_screen.dart
@@ -1,4 +1,6 @@
+import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -6,16 +8,30 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  void getLocation() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+    print('Longitude: ' + location.longitude.toString());
+    print('Latitude: ' + location.latitude.toString());
+  }
+
+  void getData() {
+    var url = Uri.https('api.openweathermap.org', '/data/2.5/weather', {'lat':,'lon':,'appid':'2120877fdf66e2a6e72b20118df6eb9e'});
+    http
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            //Get the current location
-          },
-          child: Text('Get Location'),
-        ),
+      body: Container(
+        margin: EdgeInsets.all(15),
+        color: Colors.red,
       ),
     );
   }
