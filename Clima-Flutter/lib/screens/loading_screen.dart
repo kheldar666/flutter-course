9d8007d0027d4512188be56a0ad1f5e3af388619
Martin Papy <martin.papy@gmail.com>
2021-11-30 12:14:22 +0700

    Using DotEnv

--- Clima-Flutter/lib/screens/loading_screen.dart
+++ Clima-Flutter/lib/screens/loading_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
@@ -15,9 +18,31 @@ class _LoadingScreenState extends State<LoadingScreen> {
     print('Latitude: ' + location.latitude.toString());
   }
 
-  void getData() {
-    var url = Uri.https('api.openweathermap.org', '/data/2.5/weather', {'lat':,'lon':,'appid':'2120877fdf66e2a6e72b20118df6eb9e'});
-    http
+  void getData() async {
+    var url = Uri.https(
+      'api.openweathermap.org',
+      '/data/2.5/weather',
+      {
+        'lat': '12',
+        'lon': '12',
+        'mode': 'json',
+        'appid': dotenv.env['OPENWEATHER_API_KEY'],
+      },
+    );
+
+    http.Response response = await http.get(url);
+    if (response.statusCode == 200) {
+      String data = response.body;
+      dynamic jsonData = jsonDecode(data);
+      double temperature = jsonData['main']['temp'];
+      int condition = jsonData['weather'][0]['id'];
+      String cityName = jsonData['name'];
+      print('temperature => ' + temperature.toString());
+      print('condition => ' + condition.toString());
+      print('cityName => ' + cityName.toString());
+    } else {
+      print(response.statusCode);
+    }
   }
 
   @override
@@ -28,6 +53,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold(
       body: Container(
         margin: EdgeInsets.all(15),
