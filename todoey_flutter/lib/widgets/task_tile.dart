40775f706cd49eec7f6e2dc27856a9e7889d1ffa
Martin Papy <martin.papy@gmail.com>
2021-12-06 13:06:42 +0700

    Adding Global state management

--- todoey_flutter/lib/widgets/task_tile.dart
+++ todoey_flutter/lib/widgets/task_tile.dart
@@ -1,36 +1,52 @@
 import 'package:flutter/material.dart';
 
-class TaskTitle extends StatelessWidget {
+class TaskTile extends StatefulWidget {
+  const TaskTile({Key? key}) : super(key: key);
+
+  @override
+  State<TaskTile> createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
+  void onChange(bool? newValue) {
+    setState(() {
+      isChecked = newValue!;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: const Text('Task 0',
+      title: Text('Task 0',
           style: TextStyle(
             fontSize: 18,
             fontWeight: FontWeight.w600,
+            decoration: isChecked ? TextDecoration.lineThrough : null,
           )),
-      trailing: TaskCheckbox(),
+      trailing: TaskCheckbox(
+        checkboxState: isChecked,
+        onChange: onChange,
+      ),
     );
   }
 }
 
-class TaskCheckbox extends StatefulWidget {
-  @override
-  State<TaskCheckbox> createState() => _TaskCheckboxState();
-}
+class TaskCheckbox extends StatelessWidget {
+  final bool checkboxState;
 
-class _TaskCheckboxState extends State<TaskCheckbox> {
-  bool isChecked = false;
+  final Function onChange;
+
+  const TaskCheckbox(
+      {Key? key, required this.checkboxState, required this.onChange})
+      : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Checkbox(
       activeColor: Colors.lightBlueAccent,
-      value: isChecked,
-      onChanged: (newValue) {
-        setState(() {
-          isChecked = newValue!;
-        });
-      },
+      value: checkboxState,
+      onChanged: (newValue) => onChange(newValue),
     );
   }
 }
--- todoey_flutter/lib/widgets/tasks_list.dart
+++ todoey_flutter/lib/widgets/tasks_list.dart
@@ -2,13 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:todoey_flutter/widgets/task_tile.dart';
 
 class TasksList extends StatelessWidget {
+  const TasksList({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return ListView(
       children: [
-        TaskTitle(),
-        TaskTitle(),
-        TaskTitle(),
+        TaskTile(),
+        TaskTile(),
+        TaskTile(),
       ],
     );
   }
