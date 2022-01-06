5bd93b1df25ef36bd6847c332283d427b03db79e
Martin Papy <martin.papy@gmail.com>
2022-01-06 19:00:06 +0700

    Update AppBar text

--- Expense Planner/expense_planner/lib/main.dart
+++ Expense Planner/expense_planner/lib/main.dart
@@ -23,7 +23,7 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Flutter App'),
+        title: const Text('Expense Planner'),
       ),
       body: SingleChildScrollView(
         child: Column(
