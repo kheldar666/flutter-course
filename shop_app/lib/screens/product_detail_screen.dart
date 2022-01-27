91e35c01d9bb93564316a40c63cc2df5677742bb
Martin Papy <martin.papy@gmail.com>
2022-01-27 17:18:18 +0700

    Update Product Detail screen

--- shop_app/lib/screens/product_detail_screen.dart
+++ shop_app/lib/screens/product_detail_screen.dart
@@ -12,12 +12,45 @@ class ProductDetailScreen extends StatelessWidget {
     var _productId = ModalRoute.of(context)?.settings.arguments as String;
 
     //Listen set to false will avoid to trigger the build method if the product changes
-    var _product = Provider.of<Products>(context, listen: false).findById(_productId);
+    var _product =
+        Provider.of<Products>(context, listen: false).findById(_productId);
 
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
           title: Text(_product.title),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              SizedBox(
+                height: 300,
+                width: double.infinity,
+                child: Image.network(
+                  _product.imageUrl,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              const SizedBox(height: 10),
+              Text(
+                '\$${_product.price}',
+                style: const TextStyle(
+                  color: Colors.grey,
+                  fontSize: 20,
+                ),
+              ),
+              const SizedBox(height: 10),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                width: double.infinity,
+                child: Text(
+                  _product.description,
+                  textAlign: TextAlign.center,
+                  softWrap: true,
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
