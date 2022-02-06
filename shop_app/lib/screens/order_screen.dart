98f17dba2f36a320453df7f7be58c1f488686564
Martin Papy <martin.papy@gmail.com>
2022-02-06 23:38:52 +0700

    Loading Orders in the proper screen

--- shop_app/lib/screens/order_screen.dart
+++ shop_app/lib/screens/order_screen.dart
@@ -5,10 +5,31 @@ import '/providers/orders.dart' show Orders;
 import '/widgets/order_item.dart';
 import '/widgets/shop_drawer.dart';
 
-class OrdersScreen extends StatelessWidget {
+class OrdersScreen extends StatefulWidget {
   static const routeName = '/orders';
 
   const OrdersScreen({Key? key}) : super(key: key);
+
+  @override
+  State<OrdersScreen> createState() => _OrdersScreenState();
+}
+
+class _OrdersScreenState extends State<OrdersScreen> {
+  var _isInit = false;
+  var _isLoading = false;
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    if (!_isInit) {
+      _isLoading = true;
+      Provider.of<Orders>(context).fetchOrders().then((_) {
+        setState(() {
+          _isLoading = false;
+        });
+      });
+      _isInit = true;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,10 +40,12 @@ class OrdersScreen extends StatelessWidget {
           title: const Text('Your Orders'),
         ),
         drawer: const ShopDrawer(),
-        body: ListView.builder(
-          itemCount: orderData.orders.length,
-          itemBuilder: (ctx, i) => OrderItem(orderData.orders[i]),
-        ),
+        body: _isLoading
+            ? const Center(child: CircularProgressIndicator())
+            : ListView.builder(
+                itemCount: orderData.orders.length,
+                itemBuilder: (ctx, i) => OrderItem(orderData.orders[i]),
+              ),
       ),
     );
   }
