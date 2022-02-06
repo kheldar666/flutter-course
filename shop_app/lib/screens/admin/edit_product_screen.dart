a3e963b87e48b697e38f9d8e183e0a98bfdc2f0d
Martin Papy <martin.papy@gmail.com>
2022-02-06 12:26:43 +0700

    Adding input for image URL

--- shop_app/lib/screens/admin/edit_product_screen.dart
+++ shop_app/lib/screens/admin/edit_product_screen.dart
@@ -12,6 +12,34 @@ class EditProductScreen extends StatefulWidget {
 }
 
 class _EditProductScreenState extends State<EditProductScreen> {
+  final _imageUrlController = TextEditingController();
+  final _imageUrlFocusNode = FocusNode();
+
+  @override
+  void initState() {
+    super.initState();
+    _imageUrlFocusNode.addListener(_updateImagePreview);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    // That helps prevents memory leaks
+    _imageUrlController.dispose();
+    _imageUrlFocusNode.removeListener(_updateImagePreview);
+    _imageUrlFocusNode.dispose();
+  }
+
+  void _updateImagePreview() {
+    if (!_imageUrlFocusNode.hasFocus) {
+      setState(() {});
+    }
+  }
+
+  bool _isImageUrlValid() {
+    return Uri.parse(_imageUrlController.text).isAbsolute;
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -24,6 +52,7 @@ class _EditProductScreenState extends State<EditProductScreen> {
           child: Form(
             child: SingleChildScrollView(
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: [
                   TextFormField(
                     decoration: const InputDecoration(labelText: 'Title'),
@@ -41,6 +70,50 @@ class _EditProductScreenState extends State<EditProductScreen> {
                           RegExp(r'^\d+\.?\d{0,4}'))
                     ],
                     textInputAction: TextInputAction.next,
+                  ),
+                  TextFormField(
+                    decoration: const InputDecoration(labelText: 'Description'),
+                    maxLines: 3,
+                    keyboardType: TextInputType.multiline,
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        width: 100,
+                        height: 100,
+                        margin: const EdgeInsets.only(
+                          top: 8,
+                          right: 10,
+                        ),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            width: 1,
+                            color: Colors.grey,
+                          ),
+                        ),
+                        child: !_isImageUrlValid()
+                            ? const Center(child: Text('Enter URL'))
+                            : Image.network(
+                                _imageUrlController.text,
+                                fit: BoxFit.cover,
+                              ),
+                      ),
+                      Expanded(
+                        child: TextFormField(
+                          decoration:
+                              const InputDecoration(labelText: 'Image URL'),
+                          // http://www.i2clipart.com/cliparts/8/3/d/c/clipart-book-256x256-83dc.png
+                          keyboardType: TextInputType.url,
+                          textInputAction: TextInputAction.done,
+                          controller: _imageUrlController,
+                          focusNode: _imageUrlFocusNode,
+                          onEditingComplete: () {
+                            setState(() {});
+                          },
+                        ),
+                      )
+                    ],
                   )
                 ],
               ),
